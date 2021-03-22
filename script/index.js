1104b3eecfe0dd0f14aef5d6c75da13a0b466fe7
henry-bao <henrybao@uw.edu>
2021-03-22 03:34:22 -0400

    added first page and navbar, started on the second page

---
+++ script/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const body = document.querySelector('body')
+const menu = document.querySelector('.menu-list');
+const navBar = document.querySelector('.nav');
+const menuBtn = document.querySelector('.menu-btn');
+const cancelBtn = document.querySelector('.cancel-btn');
+menuBtn.onclick = () => {
+    menu.classList.add('active');
+    menuBtn.classList.add('hide');
+    body.classList.add('disableScroll');
+
+};
+cancelBtn.onclick = () => {
+    menu.classList.remove('active');
+    menuBtn.classList.remove('hide');
+    body.classList.remove('disableScroll');
+
+};
+
+window.onscroll = () => {
+    this.scrollY > 20 ? navBar.classList.add('sticky') : navBar.classList.remove('sticky');
+};
