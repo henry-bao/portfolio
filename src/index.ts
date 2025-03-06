9f8d3d0ce24542a1aa15e3f3a0c062a9a4597ca9
henry-bao <henrybao@uw.edu>
2025-03-06 17:24:17 -0500

    nothing important

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,9 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
-import axios from 'axios';
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-app.get('/form-submit', (req, res) => {
-    if (!process.env.CONTACT_API_URL) {
-        res.status(500).send('No contact API URL set');
-        return;
-    }
-    const data = { ...req.query };
-    axios.get(`${process.env.CONTACT_API_URL}?value1=${data.value1}&value2=${data.value2}&value3=${data.value3}`);
-    res.send('Success');
-});
 
 app.use(express.static('public'));
 
